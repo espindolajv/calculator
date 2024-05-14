0214f8c3a18ca629bbb2e405a0ba18c89f81069e
espindolajv <jocanilds@gmail.com>
2024-05-14 12:16:29 -0300

    Commit

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-const input = document.getElementById('input-output')
-let outputValue = ''
-let operation1 = ''
-let operation2 = ''
+const input = document.getElementById('input-output');
+let outputValue = '';
+let operation1 = '';
+let operation2 = '';
 
-const clearBtn = document.getElementById('clear')
-clearBtn.onclick = () => input.value = ''
+const clearBtn = document.getElementById('clear');
+clearBtn.onclick = () => (input.value = '');
 
-function addValue(value){
-    input.value += value
+function addValue(value) {
+    input.value += value;
 }
 
+function calculator() {
+    outputValue = input.value;
+    input.value = '';
 
-function calculator(){
-    outputValue = input.value
-    input.value = ''
-
-    try{
-        outputValue = eval(outputValue)
-    }catch(error){
-        input.value = 'Error'
+    try {
+        outputValue = eval(outputValue);
+    } catch (error) {
+        input.value = 'Error';
     }
-
-    
 }
